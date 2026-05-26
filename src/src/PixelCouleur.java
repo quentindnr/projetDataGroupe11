eb093a6e91db79beadf717a97581db55c67a7e89
Worx <enekocgame64@gmail.com>
2026-05-26 08:58:04 +0200

    Ajout des différentes classes Pixel

---
+++ src/src/PixelCouleur.java
@@ -0,0 +1,37 @@
+public class PixelCouleur extends Pixel{
+
+    private int rouge;
+    private int vert;
+    private int bleu;
+
+    PixelCouleur(int x, int y, int rouge, int vert, int bleu) {
+        super(x, y);
+        this.rouge = rouge;
+        this.vert = vert;
+        this.bleu = bleu;
+    }
+
+    public int getRouge() {
+        return rouge;
+    }
+
+    public void setRouge(int rouge) {
+        this.rouge = rouge;
+    }
+
+    public int getVert() {
+        return vert;
+    }
+
+    public void setVert(int vert) {
+        this.vert = vert;
+    }
+
+    public int getBleu() {
+        return bleu;
+    }
+
+    public void setBleu(int bleu) {
+        this.bleu = bleu;
+    }
+}
---
+++ src/src/PixelGris.java
@@ -0,0 +1,17 @@
+public class PixelGris extends Pixel{
+
+    private int nuanceGris;
+
+    PixelGris(int x, int y, int nuanceGris) {
+        super(x, y);
+        this.nuanceGris = nuanceGris;
+    }
+
+    public int getNuanceGris() {
+        return nuanceGris;
+    }
+
+    public void setNuanceGris(int nuanceGris) {
+        this.nuanceGris = nuanceGris;
+    }
+}
