d809c7dee75c196783a67b32a5f1d9ba40d79d4d
otcast <casteraaothile@orange.fr>
2026-05-26 10:05:08 +0200

    ajout classe eigenface

---
+++ src/src/eigenface.java
@@ -0,0 +1,27 @@
+public class Eigenface{
+
+    private float valeurPropre;
+    private int rang;
+    private Vecteur vecteur;
+    
+    public Eigenface(float valeurPropre, int rang){
+        this.valeurPropre = valeurPropre;
+        this.rang = rang;
+    }
+
+    public void ajoutVecteur(Vecteur vecteur){
+        this.vecteur = vecteur;
+    }
+
+    public float getValeurPropre(){
+        return valeurPropre;
+    }
+
+    public int getRang(){
+        return rang;
+    }
+
+    public int getVecteur(){
+        return vecteur;
+    }
+}
