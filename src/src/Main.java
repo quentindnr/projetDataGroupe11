0af7ba93c43040c83f1d14349d1f56f97b69e27a
quentin <donierquen@cy-tech.fr>
2026-05-29 09:42:43 +0200

    stash

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,15 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String path = System.getProperty("user.dir") + "/archive/test.pgm";
+        String path2 = System.getProperty("user.dir") + "/archive/test.pgm";
         System.out.println("Hello World");
         Image image = new Image("image_test");
-        image.chargerImage("../archive/s1/1.pgm");
+        image.chargerImage(path);
+        Image image2 = new Image("Image_test2");
+        image2.chargerImage(path2);
         Vecteur vecteur = image.toVecteur();
+        Vecteur vecteur2 = image2.toVecteur();
         System.out.println(image.toString());
-        //System.out.println(vecteur.toString());
-        System.out.println("Nombre de dimensions du vecteur colonne : " + vecteur.getDimensions());
-        Image image2 = Image.fromVecteur(vecteur, "test");
-        System.out.println(image2.toString());
+        vecteur.produitScalaire(vecteur2);
+        System.out.print(vecteur.toString());
 
     }
 }
