e975faa1c34567f93481990790d309f55bd50813
zkayia <90650199+zkayia@users.noreply.github.com>
2026-05-29 11:52:46 +0200

    Vecteur: ajout retour distance + dimension sans s

--- src/src/Vecteur.java
+++ src/src/Vecteur.java
@@ -1,94 +1,94 @@
 public class Vecteur {
     private double[] composantes;
-    private int dimensions;
+    private int dimension;
     private int norme;
 
-    public Vecteur(double[] composantes, int dimension){
+    public Vecteur(double[] composantes, int dimension) {
         this.composantes = composantes;
-        this.dimensions = dimension;
-    }   
-    public double getComposantesAvecIndex (int i ){
+        this.dimension = dimension;
+    }
+
+    public double getComposantesAvecIndex(int i) {
         return composantes[i];
     }
 
-    public void setComposantesAvecIndex (int i, int value){
+    public void setComposantesAvecIndex(int i, int value) {
         composantes[i] = value;
     }
 
-    public int getDimensions() {
-        return dimensions;
+    public int getDimension() {
+        return dimension;
     }
 
-    public void setDimensions(int dimensions) {
-        this.dimensions = dimensions;
+    public void setDimension(int dimension) {
+        this.dimension = dimension;
     }
 
-    public void ajouter(Vecteur v){
-        if (this.dimensions != v.dimensions) {
-            throw new IllegalArgumentException("Les vecteurs doivent avoir le même nombre de dimensions pour être ajoutés.");
+    public void ajouter(Vecteur v) {
+        if (this.dimension != v.dimension) {
+            throw new IllegalArgumentException("Les vecteurs doivent avoir la même dimension pour être ajoutés.");
         }
-        for (int i = 0; i < this.dimensions; i++) {
+        for (int i = 0; i < this.dimension; i++) {
             this.composantes[i] += v.composantes[i];
         }
     }
 
-    public void soustraire(Vecteur v){
-        if (this.dimensions != v.dimensions) {
-            throw new IllegalArgumentException("Les vecteurs doivent avoir le même nombre de dimensions pour être ajoutés.");
+    public void soustraire(Vecteur v) {
+        if (this.dimension != v.dimension) {
+            throw new IllegalArgumentException("Les vecteurs doivent avoir la même dimension pour être soustraits.");
         }
-        for (int i = 0; i < this.dimensions; i++) {
+        for (int i = 0; i < this.dimension; i++) {
             this.composantes[i] += v.composantes[i];
         }
     }
 
-    public void multiplier(Vecteur v){
-        
-        if (this.dimensions != v.dimensions) {
-            throw new IllegalArgumentException("Les vecteurs doivent avoir le même nombre de dimensions pour être ajoutés.");
+    public void multiplier(Vecteur v) {
+
+        if (this.dimension != v.dimension) {
+            throw new IllegalArgumentException("Les vecteurs doivent avoir la même dimension pour être multipliés.");
         }
-        for (int i = 0; i < this.dimensions; i++) {
+        for (int i = 0; i < this.dimension; i++) {
             this.composantes[i] *= v.composantes[i];
         }
     }
 
-    public double[][] produitScalaire(Vecteur v){
-        if (this.dimensions != v.dimensions) {
-            throw new IllegalArgumentException("Les vecteurs doivent avoir le même nombre de dimensions pour être ajoutés.");
+    public double[][] produitScalaire(Vecteur v) {
+        if (this.dimension != v.dimension) {
+            throw new IllegalArgumentException("Les vecteurs doivent avoir le même nombre de dimension pour être ajoutés.");
         }
-        double[][] result = new double[this.dimensions][this.dimensions];
-        for (int i = 0; i < this.dimensions; i++) {
-            for (int j = 0; j < this.dimensions; j++) {
+        double[][] result = new double[this.dimension][this.dimension];
+        for (int i = 0; i < this.dimension; i++) {
+            for (int j = 0; j < this.dimension; j++) {
                 result[i][j] = this.composantes[i] * v.composantes[j];
             }
         }
         return result;
     }
 
-    public int getNorme(){
+    public int getNorme() {
         int sum = 0;
-        for(int i = 0; i < this.dimensions; i++){
+        for (int i = 0; i < this.dimension; i++) {
             sum += this.composantes[i] * this.composantes[i];
         }
         this.norme = (int) Math.sqrt(sum);
         return norme;
     }
 
-    public void distance(Vecteur v){
-        if (this.dimensions != v.dimensions) {
-            throw new IllegalArgumentException("Les vecteurs doivent avoir le même nombre de dimensions pour être ajoutés.");
+    public double distance(Vecteur v) {
+        if (this.dimension != v.dimension) {
+            throw new IllegalArgumentException("Les vecteurs doivent avoir la même dimension pour calculer leur distance.");
         }
         int sum = 0;
-        for (int i = 0; i < this.dimensions; i++) {
+        for (int i = 0; i < this.dimension; i++) {
             sum += Math.pow(this.composantes[i] - v.composantes[i], 2);
         }
-        double distance = Math.sqrt(sum);
-        System.out.println("La distance entre les deux vecteurs est : " + distance); 
+        return Math.sqrt(sum);
     }
 
-     @Override
-    public String toString(){
+    @Override
+    public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 0; i<this.dimensions-1; i++){
+        for (int i = 0; i < this.dimension - 1; i++) {
             stringBuilder.append(this.composantes[i]);
             stringBuilder.append("\n");
         }
